ba7887de6856d1e101f2a146964ba8f8db55a3ff
Johannes Gütschow <johannes.guetschow@pik-potsdam.de>
2022-02-09 20:15:38 +0100

    improve pydoit configuration

--- dodo.py
+++ dodo.py
@@ -4,34 +4,42 @@
 def task_update_bur():
     """ Update list of BUR submissions """
     return {
+        'targets': ['downloaded_data/UNFCCC/submissions-bur.csv'],
         'actions': ['datalad run -m "Fetch BUR submissions" '
                     '-o downloaded_data/UNFCCC/submissions-bur.csv '
                     './venv/bin/python code/UNFCCC_downloader/fetch_submissions_bur.py'],
     }
 
+
 def task_download_bur():
     """ Download BUR submissions """
     return {
+        'file_dep': ['downloaded_data/UNFCCC/submissions-bur.csv'],
         'actions': ['datalad run -m "Download BUR submissions" '
                     '-i downloaded_data/UNFCCC/submissions-bur.csv '
                     './venv/bin/python code/UNFCCC_downloader/download_bur.py'],
     }
 
+
 def task_update_nc():
     """ Update list of NC submissions """
     return {
+        'targets': ['downloaded_data/UNFCCC/submissions-nc.csv'],
         'actions': ['datalad run -m "Fetch NC submissions" '
                     '-o downloaded_data/UNFCCC/submissions-nc.csv '
                     './venv/bin/python code/UNFCCC_downloader/fetch_submissions_nc.py'],
     }
 
+
 def task_download_nc():
     """ Download NC submissions """
     return {
+        'file_dep': ['downloaded_data/UNFCCC/submissions-nc.csv'],
         'actions': ['datalad run -m "Download NC submissions" '
                     '-i downloaded_data/UNFCCC/submissions-nc.csv '
                     './venv/bin/python code/UNFCCC_downloader/download_nc.py'],
     }
+
 
 def task_download_ndc():
     """ Download NDC submissions """
