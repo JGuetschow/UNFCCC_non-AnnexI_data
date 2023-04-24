de1924b4a52b55723ca5127216613bb234332df8
Johannes Gütschow <mail@johannes-guetschow.de>
2023-04-24 09:38:55 +0200

    add CRF2023 specs (under development)

--- code/UNFCCC_CRF_reader/crf_specifications/__init__.py
+++ code/UNFCCC_CRF_reader/crf_specifications/__init__.py
@@ -4,3 +4,4 @@ Define the CRF specifications here for easy access
 
 from .CRF2021_specification import CRF2021
 from .CRF2022_specification import CRF2022
+from .CRF2023_specification import CRF2023
