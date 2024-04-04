de40f094bc66bfa6b7be9c4bf294bc98029f92b5
Daniel Busch <danielbusch@climate-resource.com>
2024-04-04 16:53:26 +0200

    assert_values function

--- UNFCCC_GHG_data/helper/functions.py
+++ UNFCCC_GHG_data/helper/functions.py
@@ -977,3 +977,48 @@ def fix_rows(
         data.loc[indices_to_merge[0]] = new_row
         data = data.drop(indices_to_merge[1:])
     return data
+
+def assert_values(
+        df : pd.DataFrame,
+        test_case : tuple[str | float | int],
+        category_column : str ="category (IPCC1996_2006_GIN_Inv)",
+        entity_column : str ='entity',
+) -> None:
+    """
+    Check if an individual value in a dataframe matches the expected value.
+
+    Input
+    -----
+    df
+        The data frame to check.
+    test_case
+        The combination of parameters and the expected value.
+        Use the format (category, entity, year, expected_value).
+    category_column
+        The columns where to look for the category.
+    entity_column
+        The column where to look for the entity.
+
+    """
+    category = test_case[0]
+    entity = test_case[1]
+    year = test_case[2]
+    expected_value = test_case[3]
+
+    assert (type(expected_value) == float) or (type(expected_value) == int), "This function only works for numbers. Use assert_nan_values to check for NaNs and empty values."
+
+    arr = df.loc[(df[category_column] == category) & (df[entity_column] == entity), year].values
+
+    # Assert the category exists in the data frame
+    assert category in df[category_column].unique(), f"{category} is not a valid category. Choose from {df[category_column].unique()}"
+
+    # Assert the category exists in the data frame
+    assert entity in df[entity_column].unique(), f"{entity} is not a valid entity. Choose from {df[entity_column].unique()}"
+
+    assert arr.size > 0, f"No value found for category {category}, entity {entity}, year {year}!"
+
+    assert arr.size <= 1, f"More than one value found for category {category}, entity {entity}, year {year}!"
+
+    assert arr[0] == test_case[3], f"Expected value {expected_value}, actual value is {arr[0]}"
+
+    print(f"Value for category {category}, entity {entity}, year {year} is as expected.")
