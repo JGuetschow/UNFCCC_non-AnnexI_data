d8ee78c16391cf0cbaddf334971e5ff5303540b3
Johannes Gütschow <mail@johannes-guetschow.de>
2024-04-05 23:13:06 +0200

    code to read Argentina BUR5 from underlying inventory (and a tiny fix for speed improvement in category aggregation)

--- UNFCCC_GHG_data/helper/__init__.py
+++ UNFCCC_GHG_data/helper/__init__.py
@@ -5,6 +5,7 @@ from .definitions import downloaded_data_path, downloaded_data_path_UNFCCC
 from .definitions import dataset_path, dataset_path_UNFCCC
 from .definitions import custom_country_mapping, custom_folders
 from .definitions import GWP_factors, gas_baskets
+from .definitions import compression
 from .functions import get_country_code, get_country_name, convert_categories
 from .functions import create_folder_mapping, process_data_for_country, get_code_file
 from .functions import fix_rows
@@ -30,4 +31,5 @@ __all__ = [
     "create_folder_mapping",
     "process_data_for_country",
     "fix_rows",
+    "compression",
 ]
