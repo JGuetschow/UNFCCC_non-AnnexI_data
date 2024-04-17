9a928f0e10444e229309016699e906f5acae4bc3
Daniel Busch <danielbusch@climate-resource.com>
2024-04-17 14:35:17 +0200

    first version

---
+++ UNFCCC_GHG_data/UNFCCC_reader/Mongolia/config_MNG_BUR2.py
@@ -0,0 +1,186 @@
+coords_terminologies = {
+    "area": "ISO3",
+    "category": "IPCC2006_PRIMAP",
+    "scenario": "PRIMAP",
+}
+
+inv_conf = {
+    "entity_row" : 0,
+    "unit_row" : 1,
+    "index_cols" : "Greenhouse gas source and sink categories",
+    "header_long" : ["orig_cat_name", "entity", "unit", "time", "data"],
+    "cat_code_regexp" : r"^(?P<code>[a-zA-Z0-9\.]{1,11})[\s\.].*",
+    "cat_codes_manual" : {
+        ' 3.C.1 -Emissions from biomass burning ' : "3.C.1",
+        "Memo Items (5)" : "MEMO",
+        "International Bunkers" : "M.BK",
+        "1.A.3.a.i - International Aviation (International Bunkers) (1)" : "M.BK.A",
+        "1.A.3.d.i - International water-borne navigation (International bunkers) (1)" : "M.BK.M",
+        "1.A.5.c - Multilateral Operations (1)(2)" : "M.MULTIOP",
+        "Total National Emissions and Removals" : "0",
+    },
+    "header" : [
+        "Greenhouse gas source and sink categories",
+        "CO2",
+        "CH4",
+        "N2O",
+        "HFCs",
+        "PFCs",
+        "SF6",
+        "other halogenated gases",
+        "Other halogenated gases without CO2 equivalent conversion factors",
+        "NOx",
+        "CO",
+        "NMVOCs",
+        "SO2",
+    ],
+    "unit" : [
+        "-",
+        "Gg",
+        "Gg",
+        "Gg",
+        "GgCO2eq",
+        "GgCO2eq",
+        "GgCO2eq",
+        "GgCO2eq",
+        "Gg",
+        "Gg",
+        "Gg",
+        "Gg",
+        "Gg",
+    ],
+}
+
+inv_conf_per_year = {
+    "1990" : {
+        "pages_to_read" : ["176", "177", "178", "179"],
+        "rows_to_fix" : {
+            3 : ['1.A.2 - Manufacturing Industries and',
+                 "2.B.4 - Caprolactam. Glyoxal and Glyoxylic Acid",
+                 "2.B.8 - Petrochemical and Carbon Black",
+                 "2.D - Non-Energy Products from Fuels and",
+                 '2.F - Product Uses as Substitutes for Ozone',
+                 "3.C - Aggregate sources and non-CO2 emissions",
+                 "3.C.4 - Direct N2O Emissions from managed",
+                 "3.C.5 - Indirect N2O Emissions from managed",
+                 "3.C.6 - Indirect N2O Emissions from manure",
+                 "5.A - Indirect N2O emissions from the atmospheric",
+                 "1.A.3.d.i - International water-borne navigation",
+                 "1.A.3.a.i - International Aviation (International",
+                 ],
+            -2 : ["3.C.1 - Emissions from biomass burning"],
+            2 : [" 3.C.1 -Emissions from biomass burning"],
+        },
+        'page_defs' : {
+            '176' : {
+                "area" : ['76,501,763,83'],
+                "cols" : ['265,320,360,396,433,471,503,564,624,658,694,741'],
+            },
+            '177' : {
+                "area" : ['68,542,762,85'],
+                "cols" : ['280,329,374,410,449,482,546,604,637,679,725,751'],
+            },
+            '178' : {
+                "area" : ['71,543,761,81'],
+                "cols" : ['265, 320,361,411,447,483,546,604,621,653,719,746'],
+            },
+            '179' : {
+                "area" : ['70,542,761,346'],
+                "cols" : ['287,328,365,410,449,482,540,600,636,675,721,750'],
+            },
+        }
+    },
+    "2020" : {
+
+        'page_defs' : {
+            '180' : {
+                "area" : ['70,436,769,86'],
+                "cols" : ['270, 322, 367, 405, 455, 488,550,607,637,669,727,753'],
+            },
+            '181' : {
+                "area" : ['68,541,768,86'],
+                "cols" : ['288,343,379,405,460,490,559,600,650,683,729,755'],
+            },
+            '182' : {
+                "area" : ['69, 539, 771, 86'],
+                "cols" : ['273,331,371,425,462,491,560,615,639,671,729,755'],
+            },
+            '183' : {
+                "area" : ['69, 540, 769, 373'],
+                "cols" : ['288, 328,363,425,459,492,560,619,650,683,731,757'],
+            },
+        },
+        "rows_to_fix" : {
+            3 : [
+                "2.B.8 - Petrochemical and Carbon Black",
+                "2.D - Non-Energy Products from Fuels and",
+                '2.F - Product Uses as Substitutes for Ozone',
+            ],
+            -2 : ['1.C.1 - Transport of CO2',
+                  '2.G.2 - SF6 and PFCs from Other Product Uses',
+                  '2.G.3 - N2O from Product Uses',
+
+                  ],
+            2 : ["3.C - Aggregate sources and non-CO2 emissions",
+                 '3.C.4 - Direct N2O Emissions from managed',
+                 "3.C.5 - Indirect N2O Emissions from managed",
+                 "3.C.6 - Indirect N2O Emissions from manure",
+                 '5.A - Indirect N2O emissions from the atmospheric',
+                 "1.A.3.d.i - International water-borne navigation",
+                 "1.A.3.a.i - International Aviation (International",
+                 '2.B.4 - Caprolactam. Glyoxal and Glyoxylic Acid',
+                 ],
+        },
+    },
+}
+
+# primap2 format conversion
+coords_cols = {
+    "category": "category",
+    "entity": "entity",
+    "unit": "unit",
+}
+
+coords_defaults = {
+    "source": "MNG-GHG-Inventory",
+    "provenance": "measured",
+    "area": "MNG",
+    "scenario": "BUR2",
+}
+
+coords_terminologies = {
+    "area": "ISO3",
+    "category": "IPCC2006_PRIMAP",
+    "scenario": "PRIMAP",
+}
+
+# TODO
+gwp_to_use = "SARGWP100"
+coords_value_mapping = {
+    "unit": "PRIMAP1",
+    "category": "PRIMAP1",
+    "entity": {
+        "HFCs": f"HFCS ({gwp_to_use})",
+        "PFCs": f"PFCS ({gwp_to_use})",
+        "SF6": f"SF6 ({gwp_to_use})",
+        "other halogenated gases" : f"other halogenated gases ({gwp_to_use})",
+        "NMVOCs": "NMVOC",
+    },
+}
+
+filter_remove = {
+    "f_memo": {"category": "MEMO"},
+    "f_empty": {"category": ""},
+    "f2": {
+        "entity": ["Other halogenated gases without CO2 equivalent conversion factors"],
+    },
+}
+
+meta_data = {
+    "references": "https://unfccc.int/documents/633382",
+    "rights": "",  # unknown
+    "contact": "daniel-busch@climate-resource.de",
+    "title": "Mongolia. Biennial update report (BUR). BUR2",
+    "comment": "Read fom pdf by Daniel Busch",
+    "institution": "UNFCCC",
+}
---
+++ UNFCCC_GHG_data/UNFCCC_reader/Mongolia/read_MNG_BUR2_from_pdf.py
@@ -0,0 +1,173 @@
+import os
+
+os.environ["UNFCCC_GHG_ROOT_PATH"] = (
+    "/Users/danielbusch/Documents/UNFCCC_non-AnnexI_data/"
+)
+
+import camelot
+import primap2 as pm2
+import pandas as pd
+
+from UNFCCC_GHG_data.helper import downloaded_data_path, extracted_data_path, fix_rows
+from config_MNG_BUR2 import (
+    inv_conf,
+    inv_conf_per_year,
+    coords_cols,
+    coords_defaults,
+    coords_terminologies,
+    coords_value_mapping,
+    filter_remove,
+    meta_data,
+)
+
+# ###
+# configuration
+# ###
+
+input_folder = downloaded_data_path / "UNFCCC" / "Mongolia" / "BUR2"
+output_folder = extracted_data_path / "UNFCCC" / "Mongolia"
+
+if not output_folder.exists():
+    output_folder.mkdir()
+
+pdf_file = "20231112_NIR_MGL.pdf"
+output_filename = "MNG_BUR2_2023_"
+category_column = f"category ({coords_terminologies['category']})"
+compression = dict(zlib=True, complevel=9)
+
+# ###
+# 1. Read in tables
+# ###
+
+df_all = None
+for year in inv_conf_per_year.keys():
+    print("-" * 60)
+    print(f"Reading year {year}.")
+    print("-" * 60)
+    df_year = None
+    for page in inv_conf_per_year[year]["page_defs"].keys():
+        print(f"Reading table from page {page}.")
+        tables_inventory_original = camelot.read_pdf(
+            str(input_folder / pdf_file),
+            pages=page,
+            table_areas=inv_conf_per_year[year]["page_defs"][page]["area"],
+            columns=inv_conf_per_year[year]["page_defs"][page]["cols"],
+            flavor="stream",
+            split_text=True,
+        )
+        print("Reading complete.")
+
+        df_page = tables_inventory_original[0].df
+
+        if df_year is None:
+            df_year = df_page
+        else:
+            df_year = pd.concat(
+                [df_year, df_page],
+                axis=0,
+                join="outer",
+            ).reset_index(drop=True)
+
+    print(f"Concatenating all tables for {year}.")
+
+    # fix content that spreads across multiple rows
+    if "rows_to_fix" in inv_conf_per_year[year]:
+        for n_rows in inv_conf_per_year[year]["rows_to_fix"].keys():
+            print(f"Merge content for {n_rows=}")
+            df_year = fix_rows(
+                df_year,
+                rows_to_fix=inv_conf_per_year[year]["rows_to_fix"][n_rows],
+                col_to_use=0,
+                n_rows=n_rows,
+            )
+
+    if year == "2020":
+        break
+
+    df_header = pd.DataFrame([inv_conf["header"], inv_conf["unit"]])
+
+    skip_rows = 11
+    df_year = pd.concat(
+        [df_header, df_year[skip_rows:]], axis=0, join="outer"
+    ).reset_index(drop=True)
+
+    df_year = pm2.pm2io.nir_add_unit_information(
+        df_year,
+        unit_row=inv_conf["unit_row"],
+        entity_row=inv_conf["entity_row"],
+        regexp_entity=".*",
+        regexp_unit=".*",
+        default_unit="Gg",
+    )
+
+    print("Added unit information.")
+
+    # set index
+    df_year = df_year.set_index(inv_conf["index_cols"])
+
+    # convert to long format
+    df_year_long = pm2.pm2io.nir_convert_df_to_long(
+        df_year, year, inv_conf["header_long"]
+    )
+
+    # extract from tuple
+    df_year_long["orig_cat_name"] = df_year_long["orig_cat_name"].str[0]
+
+    # prep for conversion to PM2 IF and native format
+    # make a copy of the categories row
+    df_year_long["category"] = df_year_long["orig_cat_name"]
+
+    # replace cat names by codes in col "category"
+    # first the manual replacements
+    # TODO not sure this is needed
+    df_year_long["category"] = df_year_long["category"].str.replace("\n", "")
+
+    df_year_long["category"] = df_year_long["category"].replace(
+        inv_conf["cat_codes_manual"]
+    )
+    # TODO not sure this is needed
+    df_year_long["category"] = df_year_long["category"].str.replace(".", "")
+
+    # then the regex replacements
+    def repl(m):
+        return m.group("code")
+
+    df_year_long["category"] = df_year_long["category"].str.replace(
+        inv_conf["cat_code_regexp"], repl, regex=True
+    )
+
+    df_year_long = df_year_long.reset_index(drop=True)
+
+    df_year_long["data"] = df_year_long["data"].str.replace(",", "")
+
+    # make sure all col headers are str
+    df_year_long.columns = df_year_long.columns.map(str)
+
+    df_year_long = df_year_long.drop(columns=["orig_cat_name"])
+
+    if df_all is None:
+        df_all = df_year_long
+    else:
+        df_all = pd.concat(
+            [df_all, df_year_long],
+            axis=0,
+            join="outer",
+        ).reset_index(drop=True)
+
+### convert to interchange format ###
+print("Converting to interchange format.")
+df_all_IF = pm2.pm2io.convert_long_dataframe_if(
+    df_all,
+    coords_cols=coords_cols,
+    coords_defaults=coords_defaults,
+    coords_terminologies=coords_terminologies,
+    coords_value_mapping=coords_value_mapping,
+    filter_remove=filter_remove,
+    meta_data=meta_data,
+    convert_str=True,
+    time_format="%Y",
+)
+
+### convert to primap2 format ###
+print("Converting to primap2 format.")
+data_pm2 = pm2.pm2io.from_interchange_format(df_all_IF)
